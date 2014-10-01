c4239d7ecc28b240e465b12aaa05827720bbe5e5
emagnus <westerga@gmail.com>
2014-10-01 18:28:30 +0200

    basic evaluator, checks how many conditions are met given a configuration

---
+++ src/main/java/no/emagnus/ulurulib/ConfigurationEvaluator.java
@@ -0,0 +1,25 @@
+package no.emagnus.ulurulib;
+
+import java.util.Collection;
+
+import no.emagnus.ulurulib.conditions.Condition;
+
+public class ConfigurationEvaluator {
+    
+    private Collection<Condition> conditions;
+
+    public ConfigurationEvaluator(Collection<Condition> conditions) {
+	this.conditions = conditions;
+    }
+    
+    public int evaluate(PieceConfiguration conf) {
+	int conditionsMet = 0;
+	for (Condition condition : conditions) {
+	    if(condition.isMet(conf)) {
+		conditionsMet++;
+	    }
+	}
+	return conditionsMet;
+    }
+
+}
